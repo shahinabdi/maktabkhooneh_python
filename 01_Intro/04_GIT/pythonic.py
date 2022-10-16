443747b760da0e0718b19661623bd308be37ee06
Shahin ABDI <contact@shahinabdi.fr>
2022-10-16 19:58:32 +0200

    Version 1.2 Pythonic for Git Test

--- 01_Intro/04_GIT/pythonic.py
+++ 01_Intro/04_GIT/pythonic.py
@@ -18,8 +18,10 @@ print(number_list3)
 list_city = ['Tehran', 'Berlin', 'Paris', 'NY']
 list_zipcode = [25, 34, 75, 16]
 
+# Dictionary Comperhension
 dict_cities2 = {k: v for k, v in zip(list_zipcode, list_city)}
 print(dict_cities2)
 
+# More Intelligent way ToDo
 dict_cities3 = dict(zip(list_zipcode, list_city))
 print(dict_cities3)
