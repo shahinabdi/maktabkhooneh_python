4d6d485a0c3828f34fc64d651aea38a0e70832c2
Shahin ABDI <contact@shahinabdi.fr>
2022-10-16 19:41:11 +0200

    Version 1.01 Pythonic for Git Test

--- 01_Intro/04_GIT/pythonic.py
+++ 01_Intro/04_GIT/pythonic.py
@@ -18,11 +18,6 @@ print(number_list3)
 list_city = ['Tehran', 'Berlin', 'Paris', 'NY']
 list_zipcode = [25, 34, 75, 16]
 
-dict_cities = dict()
-for i in range(len(list_zipcode)):
-    dict_cities[list_zipcode[i]] = list_city[i]
-print(dict_cities)
-
 dict_cities2 = {k: v for k, v in zip(list_zipcode, list_city)}
 print(dict_cities2)
 
